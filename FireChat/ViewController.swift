a015b8298d521eab657a731c66ab90993af43dca
Alejandro Rguez Cañete <ale.rodca04@gmail.com>
2023-07-17 09:21:13 +0200

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Alejandro Rodríguez Cañete on 17/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello, World!")
+    }
+
+
+}
+
