a051e93643f7d7441588aff730cf773d97e33e5f
Alejandro Rguez Cañete <ale.rodca04@gmail.com>
2023-08-11 10:17:19 +0200

    Message Model and Upload Message to Firestore

--- FireChat/Model/Message.swift
+++ FireChat/Model/Message.swift
@@ -5,9 +5,23 @@
 //  Created by Alejandro Rodríguez Cañete on 8/8/23.
 //
 
-import Foundation
+import Firebase
 
 struct Message {
     let text: String
+    let toId: String
+    let fromId: String
+    let timestamp: Timestamp!
+    var user: User?
+    
     let isFromCurrentUser: Bool
+    
+    init(dictionary: [String: AnyObject]) {
+        self.text = dictionary["text"] as? String ?? ""
+        self.toId = dictionary["toId"] as? String ?? ""
+        self.fromId = dictionary["fromId"] as? String ?? ""
+        self.timestamp = dictionary["timestamp"] as? Timestamp ?? Timestamp(date: Date())
+        
+        self.isFromCurrentUser = fromId == Auth.auth().currentUser?.uid
+    }
 }
---
+++ FireChat/Utils/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  FireChat
+//
+//  Created by Alejandro Rodríguez Cañete on 10/8/23.
+//
+
+import Firebase
+
+let COLLECTION_MESSAGES = Firestore.firestore().collection("messages")
